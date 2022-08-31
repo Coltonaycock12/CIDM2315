f49c9ed6e289672fd2cedfcefaf2ab64e2bf8e22
Coltonaycock12 <112567054+Coltonaycock12@users.noreply.github.com>
2022-08-31 15:32:05 -0500

    Colton Aycock HW1 submission

---
+++ Colton_Aycock_HW_1_8_31_22.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 2.5;
+        double y = 3.3;
+        double z = 4*x*x+3*y; 
+
+        Console.WriteLine($"x = {x}, y = {y}");
+        Console.WriteLine($"The value of z is: {z}");
+         
+        // Console.WriteLine("Hello, World!");
+        // Console.WriteLine("Colton Aycock");
+
+    }
+}
+      
